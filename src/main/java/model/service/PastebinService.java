24b17c0b377b3a554eccb5a4f45ddce47fc7e27c
qyan6939 <qyan6939@uni.sydney.edu.au>
2021-06-01 18:01:33 +0800

    [TDD RED PHASE - Get Pastebin Link Function] finish PastebinService testing

---
+++ src/main/java/model/service/PastebinService.java
@@ -0,0 +1,36 @@
+package model.service;
+
+import factory.entityfactory.*;
+import model.domain.Entity;
+import model.domain.ErrorInfo;
+import model.domain.Pastebin;
+import model.domain.User;
+import org.json.JSONObject;
+import util.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PastebinService {
+
+    private EntityCollectionFactory entityCollectionFactory;
+    private EntityFactory defaultErrorFactory;
+    private EntityFactory entityFactory;
+    private User user;
+    private PastebinAPIStrategy pastebinAPIStrategy;
+
+    public PastebinService(PastebinAPIStrategy pastebinAPIStrategy) {
+        this.pastebinAPIStrategy = pastebinAPIStrategy;
+        this.defaultErrorFactory = new DefaultErrorInfoFactory();
+        this.entityCollectionFactory = new ArticleFactory();
+    }
+
+    public Entity getPastebinLink(String token, String copiedText) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Entity e = new PastebinService(new PastebinOfflineAPIStrategy()).getPastebinLink("agr_jX9Bg7kE3-XgRLIt-TWk2teKqTxN", "PastebinOnlineAPIStrategy successful!");
+        System.out.println(e.getEntityInformation());
+    }
+}
